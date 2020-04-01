ac949c585990986114bc740a919baa2d42faa468
Matthew Jimenez <mattjimenez091@gmail.com>
2020-04-01 10:05:02 -0700

    create simple form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,7 +3,17 @@ import React, { FC } from "react";
 interface IRegisterProps {}
 
 const Register: FC<IRegisterProps> = props => {
-  return <div>Register page</div>;
+  return (
+    <>
+      <form>
+        <input placeholder="email" />
+        <input placeholder="password" />
+        <input placeholder="confirm password" />
+
+        <button>Register</button>
+      </form>
+    </>
+  );
 };
 
 export default Register;
