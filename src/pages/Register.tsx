b66d0d8733f43407f6f879922e78964aa1fadaf6
Matthew Jimenez <mattjimenez091@gmail.com>
2020-04-01 10:08:14 -0700

    initial set up of react-hook-form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,14 +1,32 @@
 import React, { FC } from "react";
 
+import { useForm } from "react-hook-form";
+
 interface IRegisterProps {}
 
 const Register: FC<IRegisterProps> = props => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <>
-      <form>
-        <input placeholder="email" />
-        <input placeholder="password" />
-        <input placeholder="confirm password" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name="email" type="email" placeholder="email" ref={register} />
+
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          ref={register}
+        />
+
+        <input
+          name="confirm"
+          type="password"
+          placeholder="confirm password"
+          ref={register}
+        />
 
         <button>Register</button>
       </form>
